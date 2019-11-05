92dde481254726d7ee76446e4345b41902eec2cd
Francisco Farias <fariasfranciscoe@gmail.com>
2019-11-05 16:12:30 -0300

    Update sorting.js

--- sorting.js
+++ sorting.js
@@ -1,159 +1,159 @@
 function selectionSort (array) {
-	const l = array.length
-	let index, aux, i, j;
+  const l = array.length
+  let index, aux, i, j;
 
-	for (i = 0; i < l; i++) {
-		index = i;
+  for (i = 0; i < l; i++) {
+    index = i;
 
-		for (j = i; j < l; j++){
-			if (array[j] < array[index]) index = j
-		}
+    for (j = i; j < l; j++){
+      if (array[j] < array[index]) index = j
+    }
 
-		aux = array[index];
+    aux = array[index];
 
-		array.splice(index, 1);
-		array.splice(i, 0, aux);
-	}
+    array.splice(index, 1);
+    array.splice(i, 0, aux);
+  }
 }
 
 function bubbleSort (array) {
-	const l  = array.length - 1;
-  	let i, aux, swapped = true;
+  const l  = array.length - 1;
+  let i, aux, swapped = true;
   
-  	while (swapped) {
-  		swapped = false;
+  while (swapped) {
+    swapped = false;
 
-	  	for (i = 0; i < l; i++) {
-	  		if (array[i] > array[i + 1]) {
-		  		aux = array[i];
-		  		array[i] = array[i + 1];
-		  		array[i + 1] = aux;
-		  		swapped = true;
-		  	}
-	    }
-	}
+    for (i = 0; i < l; i++) {
+      if (array[i] > array[i + 1]) {
+        aux = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = aux;
+        swapped = true;
+      }
+    }
+  }
 }
 
 function insertSort (array) {
-	const l = array.length;
-	let i, j, aux, swapped;
+  const l = array.length;
+  let i, j, aux, swapped;
 
-	for (i = 1; i < l; i++) {
-		swapped = false;
+  for (i = 1; i < l; i++) {
+    swapped = false;
 
-		if (array[i] < array[i - 1]) {
-			for (j = i - 2; j > -1; j--) {
-				if (array[i] > array[j]) {
-					aux = array[i];
+    if (array[i] < array[i - 1]) {
+      for (j = i - 2; j > -1; j--) {
+        if (array[i] > array[j]) {
+          aux = array[i];
 
-					array.splice(i, 1);
-					array.splice(j + 1, 0, aux);
+          array.splice(i, 1);
+          array.splice(j + 1, 0, aux);
 
-					swapped = true;
+          swapped = true;
 
-					break;
-				}
-			}
+          break;
+        }
+      }
 
-			if (!swapped) {
-				aux = array[i];
+      if (!swapped) {
+        aux = array[i];
 
-				array.splice(i, 1);
-				array.splice(0, 0, aux);
-			}
-		}
-	}
+        array.splice(i, 1);
+        array.splice(0, 0, aux);
+      }
+    }
+  }
 }
 
 function heapSort (array) {
-	function getLeftChildIndex (index) {
-		return index * 2 + 1;
-	}
+  function getLeftChildIndex (index) {
+    return index * 2 + 1;
+  }
 
-	function hasLeftChild (index) {
-		return getLeftChildIndex(index) < array.length;
-	}
+  function hasLeftChild (index) {
+    return getLeftChildIndex(index) < array.length;
+  }
 
-	function getLeftChild (index) {
-		return array[getLeftChildIndex(index)];
-	}
+  function getLeftChild (index) {
+    return array[getLeftChildIndex(index)];
+  }
 
-	function getRightChildIndex (index) {
-		return index * 2 + 2;
-	}
+  function getRightChildIndex (index) {
+    return index * 2 + 2;
+  }
 
-	function hasRightChild (index) {
-		return getRightChildIndex(index) < array.length;
-	}
+  function hasRightChild (index) {
+    return getRightChildIndex(index) < array.length;
+  }
 
-	function getRightChild (index) {
-		return array[getRightChildIndex(index)];
-	}
+  function getRightChild (index) {
+    return array[getRightChildIndex(index)];
+  }
 
-	function getParentIndex (index) {
-		return Math.floor((index -1) / 2);
-	}
+  function getParentIndex (index) {
+    return Math.floor((index -1) / 2);
+  }
 
-	function hasParent (index) {
-		return getParentIndex(index) >= 0;
-	}
+  function hasParent (index) {
+    return getParentIndex(index) >= 0;
+  }
 
-	function getParent (index) {
-		return array[getParentIndex(index)];
-	}
+  function getParent (index) {
+    return array[getParentIndex(index)];
+  }
 
-	function swapValues (i, j) {
-		const aux = array[i];
+  function swapValues (i, j) {
+    const aux = array[i];
 
-		array[i] = array[j];
-		array[j] = array[aux];
-	}
+    array[i] = array[j];
+    array[j] = array[aux];
+  }
 
-	function peek () {
-		if (array.length < 1) throw new Error('Size');
+  function peek () {
+    if (array.length < 1) throw new Error('Size');
 
-		return array[0];
-	}
+    return array[0];
+  }
 
-	function pool () {
-		const item = peek();
+  function pool () {
+    const item = peek();
 
-		array[0] = array[array.length - 1];
-		array.length--;
+    array[0] = array[array.length - 1];
+    array.length--;
 
-		heapifyDown();
+    heapifyDown();
 
-		return item;
-	}
+    return item;
+  }
 
-	function add (item) {
-		array.push(item);
-		heapifyUp();
-	}
+  function add (item) {
+    array.push(item);
+    heapifyUp();
+  }
 
-	function heapifyUp () {
-		let index = array.length - 1;
+  function heapifyUp () {
+    let index = array.length - 1;
 
-		while (hasParent(index) && getParent(index) > array[index]) {
-			swapValues(getParentIndex(index), index);
+    while (hasParent(index) && getParent(index) > array[index]) {
+      swapValues(getParentIndex(index), index);
 
-			index = getParentIndex(index);
-		}
-	}
+      index = getParentIndex(index);
+    }
+  }
 
-	function heapifyDown () {
-		let index = 0, smallerChildIndex;
+  function heapifyDown () {
+    let index = 0, smallerChildIndex;
 
-		while (hasLeftChild(index)) {
-			smallerChildIndex = hasRightChild(index) && getRightChild(index) < getLeftChild(index) ? getRightChildIndex(index) : getLeftChildIndex(index);
+    while (hasLeftChild(index)) {
+      smallerChildIndex = hasRightChild(index) && getRightChild(index) < getLeftChild(index) ? getRightChildIndex(index) : getLeftChildIndex(index);
 
-			if (array[index] < array[smallerChildIndex]) {
-				break;
-			} 
+      if (array[index] < array[smallerChildIndex]) {
+        break;
+      } 
 
-			swapValues(index, smallerChildIndex);
+      swapValues(index, smallerChildIndex);
 			
-			index = smallerChildIndex;
-		}
-	}
+      index = smallerChildIndex;
+    }
+  }
 }
