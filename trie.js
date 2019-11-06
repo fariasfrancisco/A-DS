0bc97ce5c600f6ea45620c7123c61d8dcce90a2d
Francisco Farias <fariasfranciscoe@gmail.com>
2019-11-06 01:56:42 -0300

    Add files via upload

---
+++ trie.js
@@ -0,0 +1,83 @@
+class Node {
+	constructor(char, children, isWord, count) {
+		this.char = char;
+		this.children = children || new Map();
+		this.isWord = isWord || false;
+		this.count = count || 0;
+	}
+
+	getChild (char) {
+		return this.children.get(char);
+	}
+
+	setChild (char, node) {
+		this.children.set(char, node);
+	}
+
+	getNodeByString (string) {
+		if (string.length === 1) return this.getChild(string);
+	
+		const child = this.getChild(string.charAt(0));
+
+		if (child == null) return;
+
+		return child.getNodeByString(string.slice(1));
+	}
+}
+
+// Implement Contact list:
+// - Add contact
+// - Given a string, return how many contacts begin with that string
+
+
+class Trie {
+	constructor() {
+		this.root = new Node('*');
+	}
+
+	addString (string) {
+		const l = string.length;
+		let node = this.root;
+		let child;
+
+		for (let i = 0; i < l; i++) {
+			child = node.getChild(string.charAt(i));
+
+			if (child == null) {
+				child = new Node(string.charAt(i));
+
+				node.setChild(string.charAt(i), child)
+			}
+
+			node = child;
+		}
+
+		node.isWord = true
+		node.count++;
+	}
+
+	findCount (string) {
+		const node = this.root.getNodeByString(string);
+
+		if (node == null) return 0;
+
+		return Trie.count(node);
+	}
+
+	static count (node) {
+		const iterator = node.children.entries();
+		let c = node.count;
+		let next = iterator.next().value
+
+		while (next != null) {
+			c += Trie.count(next[1]);
+			next = iterator.next().value;
+		}
+
+		return c;
+	}
+}
+
+const trie = new Trie();
+trie.addString('Fran');
+trie.findCount('Fr');
