9528762f1d666c0e64ac1dfdc3e9bdc1411a3080
Francisco Farias <fariasfranciscoe@gmail.com>
2019-11-06 23:07:13 -0300

    Add files via upload

---
+++ ransomNote.js
@@ -0,0 +1,18 @@
+function canWriteNote (magazine, note) {
+  const magazineMap = new Map();
+  let count;
+  
+  for (const word of magazine) {
+    magazineMap.set(word, magazineMap.has(word) ? magazineMap.get(word) + 1 : 1);
+  }
+
+  for (const word of note) {
+    count = magazineMap.get(word);
+
+    if (count == null || count < 1) return false;
+
+    magazineMap.set(word, count - 1);
+  }
+
+  return true;
+}
