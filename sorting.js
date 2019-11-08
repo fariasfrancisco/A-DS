95425e1c1ac7fbf7e32bc54c856ada1cfc1d60dc
Francisco Farias <fariasfranciscoe@gmail.com>
2019-11-08 20:59:13 -0300

    added gitignore and more sorting

--- sorting.js
+++ sorting.js
@@ -1,11 +1,11 @@
-function selectionSort (array) {
-  const l = array.length
+function selectionSort(array) {
+  const l = array.length;
   let index, aux, i, j;
 
   for (i = 0; i < l; i++) {
     index = i;
 
-    for (j = i; j < l; j++){
+    for (j = i; j < l; j++) {
       if (array[j] < array[index]) index = j
     }
 
@@ -16,10 +16,10 @@ function selectionSort (array) {
   }
 }
 
-function bubbleSort (array) {
-  const l  = array.length - 1;
+function bubbleSort(array) {
+  const l = array.length - 1;
   let i, aux, swapped = true;
-  
+
   while (swapped) {
     swapped = false;
 
@@ -34,7 +34,7 @@ function bubbleSort (array) {
   }
 }
 
-function insertSort (array) {
+function insertSort(array) {
   const l = array.length;
   let i, j, aux, swapped;
 
@@ -63,4 +63,95 @@ function insertSort (array) {
       }
     }
   }
+}
+
+// O(n log n)
+function quickSort(array) {
+  function sortPartition(array, left, right) {
+    if (left >= right) return;
+
+    const pivot = array[Math.floor((left + right) / 2)];
+    const index = partition(array, left, right, pivot);
+
+    sortPartition(array, left, index - 1);
+    sortPartition(array, index, right);
+  }
+
+  function partition(array, left, right, pivot) {
+    while (left <= right) {
+      while (array[left] < pivot) {
+        left++;
+      }
+
+      while (array[right] > pivot) {
+        right--;
+      }
+
+      if (left <= right) {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+      }
+    }
+
+    return left;
+  }
+
+  sortPartition(array, 0, array.length - 1);
+}
+
+// O(n log n) time
+// O(n) space
+function mergeSort(arr) {
+  function merge(lHalf, rHalf, array) {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    const l = lHalf.length;
+    const r = rHalf.length;
+
+    while (i < l && j < r) {
+      if (lHalf[i] < rHalf[j]) {
+        array[k] = lHalf[i];
+
+        i++;
+      } else {
+        array[k] = rHalf[j];
+
+        j++;
+      }
+
+      k++;
+    }
+
+    while (i < l) {
+      array[k] = lHalf[i];
+
+      i++;
+      k++;
+    }
+
+    while (j < r) {
+      array[k] = rHalf[j];
+
+      j++;
+      k++;
+    }
+  }
+
+  function sort(array) {
+    const l = array.length;
+
+    if (l < 2) return;
+
+    const mid = Math.floor(l / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
+
+    sort(left);
+    sort(right);
+    merge(left, right, array)
+  }
+
+  sort(arr);
 }
